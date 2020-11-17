fee55b824cec7762eafbb223033aa34addda187d
JoseG-CF <72420397+JoseG-CF@users.noreply.github.com>
2020-11-16 19:42:23 -0600

    Added objects to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello world');
-let favoriteFood = 'Waffles';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 2.04,
+    type: ['grass', 'poison'],
+  },
+  {
+    name: "Charizard",
+    height: 5.07,
+    type: ['fire','flying'],
+  },
+  {
+    name: "Butterfree",
+    height: 3.07,
+    type: ['bug','flying'],
+  },
+];
